c3a1f18fb4c4e23d0e570e060bbe15064abb1ef5
Alexxelaa <lesha3mod@gmail.com>
2023-07-05 16:39:54 +0300

    lesson5task1
    task1

---
+++ lesson5task1.py
@@ -0,0 +1,13 @@
+value1 = int(input())
+symbol = str(input())
+value2 = int(input())
+if symbol == "+":
+    print(value1 + value2)
+elif symbol == "-":
+    print(value1 - value2)
+elif symbol == "*":
+    print(value1 * value2)
+elif symbol == "/":
+    print(value1 / value2)
+elif symbol == "**":
+    print(value1 ** value2)
