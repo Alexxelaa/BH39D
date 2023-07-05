16c6868a55912f1780584fb6e0a5edc534a1f0da
Alexxelaa <lesha3mod@gmail.com>
2023-07-05 15:24:25 +0300

    lesson1task3
    task3

---
+++ lesson1task3.py
@@ -0,0 +1,18 @@
+#номер 3
+#1 способ
+name = input()
+age = input()
+city = input()
+print(f"Привет {age}-летний {name} из города {city}")
+
+#2 способ
+#name = input()
+#age = input()
+#city = input()
+#print("Привет {}-летний {} из города {}".format(age, name, city))
+
+#3 способ
+# name = input()
+# age = input()
+# city = input()
+# print("Привет %s-летний %s из города %s" % (age, name, city))
